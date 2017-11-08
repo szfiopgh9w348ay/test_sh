9e1bd6d90d7f59aea13ec3366c6659dd2e0a379e
Shaoyan Li <sebastianzimmeck@ISRs-Air.wv.cc.cmu.edu>
2017-11-08 13:00:12 -0500

    asdf

--- compliance_system_ci.sh
+++ compliance_system_ci.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+set -x
+apk add --no-cache curl curl-dev bash py-pip
+pip install docker-compose
+docker-compose --version
 cd ../compliance-system-repo && \
 cp dot_env_example .env && \
 ./deploy.sh TEST && \
