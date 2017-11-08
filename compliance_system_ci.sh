f4e58438cf14be88acff5a4b0c9be479b5b0dc39
Shaoyan Li <sebastianzimmeck@ISRs-Air.wv.cc.cmu.edu>
2017-11-08 13:02:05 -0500

    asdf

--- compliance_system_ci.sh
+++ compliance_system_ci.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
+cd ../compliance-system-repo
 set -x
 apk add --no-cache curl curl-dev bash py-pip
 pip install docker-compose
 docker-compose --version
-cd ../compliance-system-repo && \
 cp dot_env_example .env && \
 ./deploy.sh TEST && \
 docker-compose exec django pytest -v
